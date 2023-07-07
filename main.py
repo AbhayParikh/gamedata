551b859233273552a44ce10c94f53724eb039443
AbhayParikh <138758011+AbhayParikh@users.noreply.github.com>
2023-07-07 10:31:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,4 +7,4 @@ app.include_router(game_data.router)
 
 
 if __name__ == '__main__':
-    uvicorn.run("main:app", host='127.0.0.1', port=8000, log_level="info", reload=True)
+    uvicorn.run("main:app", host='0.0.0.0', port=10000, log_level="info", reload=True)
